f3ea82da9d3af1ed0e42d71a5cad97b10eb1cdbe
Bibhutibhusan behera <bibhuti2000behera@gmail.com>
2022-10-19 19:37:48 +0530

    Submitted

--- src/Number.java
+++ src/Number.java
@@ -3,7 +3,7 @@ public class Number {
     public static void main(String[] args)
     {
         int num = 1234;
-        int f1=num%10,p1=(num%10)*1;
+        int f1=num%10,p1=(num%10);
         num=num/10;
         int f2=num%10,p2=(num%10)*10;
         num=num/10;
